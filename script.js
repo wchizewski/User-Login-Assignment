b62c1fd5159daf34ab3ba14300063f55601627fd
wchizewski <w.chizewski@share.epsb.ca>
2021-09-21 14:48:20 -0600

    assignemehnt

---
+++ script.js
@@ -0,0 +1,11 @@
+// Login Assignment by Will
+
+// Event Listener
+document.getElementById("loginBtn").addEventListener("click", login);
+
+// Event Function
+function login() {
+    let username = document.getElementById()
+}
+
+
