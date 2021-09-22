296399971e8563bfc124b02f21754ed1313407d4
wchizewski <w.chizewski@share.epsb.ca>
2021-09-22 14:01:00 -0600

    login assingment odne

--- script.js
+++ script.js
@@ -5,7 +5,16 @@ document.getElementById("loginBtn").addEventListener("click", login);
 
 // Event Function
 function login() {
-    let username = document.getElementById()
+    let username = document.getElementById("userIn").value;
+    let password = document.getElementById("passIn").value;
+
+    if (username === "admin" && password === "1234") {
+        alert("Login Successful");
+    } else if(username === "" && password === ""){
+        alert("Please enter your username and password")
+    } else if(username !== "admin"){
+        alert("Invalid Username")
+    } else if(password !== "1234") {
+        alert("Invalid Password")
+    }
 }
-
-
